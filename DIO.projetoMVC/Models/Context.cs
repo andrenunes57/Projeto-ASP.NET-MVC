9d0b8f4991a26edb8f0912f0aa96fcf593b617a9
André Nunes de Lima <andre.nunes@live.com>
2021-06-18 00:14:53 -0300

    Criado as classe de Categorias e Produtos para realizar operações CRUD em conexão com o Banco de Dados

---
+++ DIO.projetoMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO.projetoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-T16M5FS\SQLEXPRESS;Initial Catalog=DIOProjetoMVC;Integrated Security=True");
+        }
+    }
+}
